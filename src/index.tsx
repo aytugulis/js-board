17bc9df24cfe5ffa8112ddeba2237de933ab1bd1
AYTU ULIŞ <aytugulis@gmail.com>
2022-03-27 13:49:31 +0300

    Creating redux pattern

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,20 @@
 import ReactDOM from "react-dom";
 import "bulmaswatch/superhero/bulmaswatch.min.css";
 
+import { Provider } from "react-redux";
+import { store } from "./state";
+
 import { CodeCell } from "./components/CodeCell/CodeCell";
 import { TextEditor } from "./components/TextEditor/TextEditor";
 
 const App = () => {
   return (
-    <div>
-      {/* <CodeCell /> */}
-      <TextEditor />
-    </div>
+    <Provider store={store}>
+      <div>
+        {/* <CodeCell /> */}
+        <TextEditor />
+      </div>
+    </Provider>
   );
 };
 
---
+++ src/state/reducers/cellsReducer.ts
@@ -0,0 +1,51 @@
+import { Cell } from "./../cell";
+import { Action } from "./../actions/index";
+import { ActionType } from "../action-types";
+
+interface CellsState {
+  loading: boolean;
+  error: string | null;
+  order: string[];
+  data: {
+    [key: string]: Cell;
+  };
+}
+
+const initialState: CellsState = {
+  loading: false,
+  error: null,
+  order: [],
+  data: {},
+};
+
+const reducer = (
+  state: CellsState = initialState,
+  action: Action
+): CellsState => {
+  switch (action.type) {
+    case ActionType.UPDATE_CELL:
+      const { id, content } = action.payload;
+      return {
+        ...state,
+        data: {
+          ...state.data,
+          [id]: {
+            ...state.data[id],
+            content: content,
+          },
+        },
+      };
+    case ActionType.DELETE_CELL:
+      return state;
+    case ActionType.MOVE_CELL:
+      return state;
+    case ActionType.INSERT_CELL_BEFORE:
+      return state;
+    default:
+      return state;
+  }
+
+  return state;
+};
+
+export default reducer;
