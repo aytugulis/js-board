718aa5de800c9d5beb88189d22663e6e3eee7e28
AYTU ULIŞ <aytugulis@gmail.com>
2022-03-27 14:25:18 +0300

    Implementing cellsReducer with immer

--- src/state/reducers/cellsReducer.ts
+++ src/state/reducers/cellsReducer.ts
@@ -1,6 +1,7 @@
 import { Cell } from "./../cell";
 import { Action } from "./../actions/index";
 import { ActionType } from "../action-types";
+import produce from "immer";
 
 interface CellsState {
   loading: boolean;
@@ -18,34 +19,52 @@ const initialState: CellsState = {
   data: {},
 };
 
-const reducer = (
-  state: CellsState = initialState,
-  action: Action
-): CellsState => {
+const reducer = produce((state: CellsState = initialState, action: Action) => {
   switch (action.type) {
     case ActionType.UPDATE_CELL:
       const { id, content } = action.payload;
-      return {
-        ...state,
-        data: {
-          ...state.data,
-          [id]: {
-            ...state.data[id],
-            content: content,
-          },
-        },
-      };
-    case ActionType.DELETE_CELL:
-      return state;
-    case ActionType.MOVE_CELL:
-      return state;
-    case ActionType.INSERT_CELL_BEFORE:
-      return state;
-    default:
-      return state;
-  }
 
-  return state;
-};
+      state.data[id].content = content;
+
+      break;
+    case ActionType.DELETE_CELL:
+      delete state.data[action.payload];
+      state.order = state.order.filter((id) => id !== action.payload);
+
+      break;
+    case ActionType.MOVE_CELL:
+      const { direction } = action.payload;
+      const index = state.order.findIndex((id) => id === action.payload.id);
+      const targetIndex = direction === "up" ? index - 1 : index + 1;
+
+      if (targetIndex < 0 || targetIndex > state.order.length - 1) break;
+
+      state.order[index] = state.order[targetIndex];
+      state.order[targetIndex] = action.payload.id;
+
+      break;
+    case ActionType.INSERT_CELL_BEFORE:
+      const cell: Cell = {
+        content: "",
+        type: action.payload.type,
+        id: randomId(),
+      };
+
+      state.data[cell.id] = cell;
+
+      const foundIndex = state.order.findIndex(
+        (id) => id === action.payload.id
+      );
+
+      if (foundIndex < 0) state.order.push(cell.id);
+      else state.order.splice(foundIndex, 0, cell.id);
+
+      break;
+    default:
+      break;
+  }
+}, initialState);
+
+const randomId = () => Math.random().toString(36).substring(2, 5);
 
 export default reducer;
